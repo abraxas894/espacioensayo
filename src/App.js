6702bd16ab9f55af0dd59ac8c27e030530b8c1a3
abraxas894 <artd_kda@hotmail.com>
2024-03-08 20:27:09 -0600

    Se realizó la actividad integradora con un diseño que debe ser revisado

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="cabeza">
+        <h1>Voces del eco presente</h1>
+      </header>
+      <div className="portada">
+      </div>
+      <main>
+        <h2>08 de marzo</h2>
+        <p>
+          Me gustaría descubrir una palabra todos los días; es decir, si el diccionario tiene un número determinado de páginas, sería posible transitar completamente el lenguaje que resguarda. No suelo ser metódico, abundan las celadas cuando pretendo despejar la bruma matutina que encadena mis extremidades; prefiero conciliar mis sensaciones hacia las rutinas abiertas: agitar las manos sobre la fugacidad de un esplendor. La palabra de hoy sería <i>catódica</i>, la encontré mientras revisaba la Enciclopedia Hispánica. Me encontraba ahí porque buscaba la acepción de <i>rectángulo</i>; encontré <i>rayos catódicos</i> y sentí que podía funcionar bien. La palabra de hoy tendría que ser <i>cátodo</i> pero como dije, no estoy aquí para ser meticuloso. Quería rectángulos porque consideré que se trata de una palabra artificial: una recta se encuentra con el ángulo de otra recta hasta regresar a su origen (siempre y cuando los ángulos sean de 90°). <i>Rectángulo</i> no tiene la misma articulación seglar de otras palabras deformadas por nuestra lengua. Pensaba en rectángulos mientras limpiaba el patio, cuestionaba mi esfuerzo por dejar resplandeciente el piso blanco, utilizar cubeta y media de agua reciclada y tratar de estirar las abreviaturas de mi destino frente a la escoba y la fusión de cloro con jabón. Pensaba en rectángulos porque nuestros espacios funcionan en bloques, extensiones angulares de 90°: transitamos sobre pantallas, páginas, pisos y pastillas rectangulares. Es lógico que nuestro horizonte mantenga proporciones con la misma artificialidad de la palabra. ¿Qué tan complicado sería saber el área de un rectángulo? Creo que si nuestra espacialidad fuera más disruptiva, nuestra mente podría explorar zonas más sugestivas, expandir sus latitudes para reconocer figuras disueltas. Lo pensé porque desafiaba cierta nubosidad mientras limpiaba los pisos: no fui aceptado como corresponsal dentro de una revista digital. El programa era simple y sentí que mi perfil se ajustaba a los requisitos; no ser aceptado fue un pequeño movimiento lacerante: un alfiler comparado con haber perdido mi lugar para estudiar y trabajar en la edición hace cerca de un año. Mientras tendía mi ropa llegaron las turbaciones: sé que hace un año lo perdí por mi culpa, por no brindar las entrevistas requeridas y los perfiles de cada escenario. El cielo estaba despejado, mi pechito también mientras tomaba las pinzas y acomodaba mi ropa. ¿Será porque tengo 33 años o soy de la Ciudad de México? ¿Será porque la convocatoria pedía estar en 4° semestre? ¿Será que escribo feo? Por lo anterior, sentí que requería nuevas palabras para afianzar mis manos sobre la madera de la escoba. Quizá ese es mi destino: barrer y sólo barrer. Barrer los espacios rectangulares, escudriñar sus esquinas y fracasar contra la guerra del polvo. Creo que nuestra concepción del firmamento no se describe dentro de la planicie de un rectángulo, el reino de lo desconocido conserva una forma oblicua, trazada por el deslizamiento de la duda y del asombro. Quizá deba seguir barriendo, buscar la fórmula del cloro etéreo para desinfectarme de lo hegemónico y la imposición positiva de los absolutos. Dentro del patio, los pisos distantes al centro están brumosos, teñidos por el color del tiempo y manchados con matices que desconocen los ángulos rectos: esas manchas comparten vicisitudes con el universo. Siento que la escritura debe ser así, un espacio con una área complicada de calcular; no se trata de un espacio seguro delimitado por el ancho de nuestra experiencia y la altura de nuestra perspectiva. Pertenezco a una generación que escribe y vive para resguardarse de la agitación del mundo; se espera que sus textos tengan siempre el mismo ángulo y regresen al mismo punto de origen. Es de madrugada y no tengo sueño, he puesto la alarma a las ocho de la mañana. Hace una semana no la necesité, dormí dos horas luego de soñar con la visitante nocturna; si vuelve aparecer, me dejaré seducir por la extensión de sus signos. Cuando pretenda arrinconar mi alma en una esquina, le leeré este párrafo que me gustó sobre los rayos catódicos:
+        </p>
+        <blockquote>
+          ❡ Los rayos catódicos son descargas eléctricas producidas en el interior de tubos llenos de gas enrarecido y sometidos a una diferencia de potencial eléctrico existente entre sus extremos metálicos o polos. La radiación emerge desde el polo positivo o cátodo del dispositivo, y se halla construida por electrones o partículas elementales de naturaleza eléctrica negativa que se propagan a modo de haces por el interior del tubo.
+        </blockquote>
+        <p>
+          A modo de haces, nos propagamos por el interior de la vida.
+        </p>
+
+      </main>
+        
+    </div>
+  );
+}
+
+export default App;
